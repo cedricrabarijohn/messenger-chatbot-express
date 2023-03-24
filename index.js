4402ffd4715c1d82af15e5af6865e10ec84b7518
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2023-03-25 02:04:33 +0300

    test bot

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-let express = require("express")
-const bodyParser = require("body-parser")
-const dotenv = require('dotenv')
-dotenv.config()
-const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+var app = require("express")();
+var server = require("http").Server(app);
+var MessengerPlatform = require("facebook-bot-messenger");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+var bot = MessengerPlatform.create({
+  pageID: process.env.PAGE_ID,
+  appID: process.env.APP_ID,
+  appSecret: process.env.APP_SECRET,
+  validationToken: process.env.VALIDATION_TOKEN,
+  pageToken: process.env.PAGE_TOKEN,
+});
 
-app.get("/", (req, res) => res.send(`Hello World! ${process.env.TEST_VARIABLE}`));
+app.use(bot.webhook("/webhook"));
+bot.on(MessengerPlatform.Events.MESSAGE, function (userId, message) {
+  // add code below.
+  bot.sendTextMessage(userId, "Hello You");
+});
+server.listen(8080);
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+// bot.sendTextMessage(userId, "Hello You");
