398d1b89f9c6600fe5e27c9b65e296cd4e7aa73f
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2023-03-25 01:12:41 +0300

    qwdqw

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-"use strict";
-let express = require("express"),
-bodyParser = require("body-parser"),
-app = express();
+let express = require("express")
+const bodyParser = require("body-parser")
+const dotenv = require('dotenv')
+dotenv.config()
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.get("/", (req, res) => res.send(`Hello World! ${process.env.TEST_VARIABLE}`));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
